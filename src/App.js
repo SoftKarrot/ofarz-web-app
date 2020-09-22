7af6bb36b858d5118cd5f3c513fa2b5876f1bd3b
Sohan021 <sohanrahman021@gmail.com>
2020-09-22 11:08:36 +0600

    category

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route } from "react-router-dom";
+import NavbarComponent from './components/common/NavbarComponent';
+import CreateCategoryContainer from './containers/categoryContainers/CreateCategoryContainer';
+import DetailCategoryContaioner from './containers/categoryContainers/DetailCategoryContaioner';
+import EditCategoryContainer from './containers/categoryContainers/EditCategoryContainer';
+import HomeContainer from './containers/categoryContainers/CategoryHomeContainer';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <NavbarComponent />
+      <BrowserRouter>
+        <Route path="/" exact component={HomeContainer} />
+
+        <Route path="/create" exact component={CreateCategoryContainer} />
+
+        <Route path="/detail/:id" exact component={DetailCategoryContaioner} />
+
+        <Route path="/edit/:id" exact component={EditCategoryContainer} />
+      </BrowserRouter>
     </div>
   );
 }
